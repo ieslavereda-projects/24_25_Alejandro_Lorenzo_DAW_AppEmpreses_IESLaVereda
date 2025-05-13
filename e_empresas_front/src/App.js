621496b5167983ffa81f5fb86c94f7a26c49bc2b
alorenzot <alejandrolorenzotoledo@gmail.com>
2025-05-13 22:11:36 +0200

    Creadas algunas rutas para testeos

--- e_empresas_front/src/App.js
+++ e_empresas_front/src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import AddUserPage from './pages/AddUserPage';
+import UserPage from './pages/UserPage';
 import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import ImportTutors from './pages/ImportTutors';
+import UserForm from './components/UserForm';
+import ViewUser from './components/ViewUser';
+import EditUser from './components/EditUser';
 
 function App() {
   return (
@@ -14,9 +18,37 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
 
         <Route
+          path="/user-menu"
+          element={
+            <ProtectedRoute requiredRole="admin" element={<UserPage />} />
+          }
+        />
+
+        <Route
+          path="/users/:id"
+          element={
+            <ProtectedRoute requiredRole="admin" element={<ViewUser />} />
+          }
+        />
+
+        <Route
+          path="/users/:id/edit"
+          element={
+            <ProtectedRoute requiredRole="admin" element={<EditUser />} />
+          }
+        />
+
+        <Route
           path="/add-user"
           element={
-            <ProtectedRoute requiredRole="admin" element={<AddUserPage />} />
+            <ProtectedRoute requiredRole="admin" element={<UserForm />} />
+          }
+        />
+
+        <Route
+          path="/import-tutors"
+          element={
+            <ProtectedRoute requiredRole="admin" element={<ImportTutors />} />
           }
         />
         
