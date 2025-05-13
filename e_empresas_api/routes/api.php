31500c5aa5a4e33bdcfe7d13c421a8a6a9bfa5da
alorenzot <alejandrolorenzotoledo@gmail.com>
2025-05-13 22:12:58 +0200

    Añadidas rutas necesarias

--- e_empresas_api/routes/api.php
+++ e_empresas_api/routes/api.php
@@ -2,17 +2,42 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\TutorsImportController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CompanyDeregisterRequestController;
+use App\Http\Controllers\CompanyRegisterRequestController;
+use App\Http\Controllers\CompanyReviewController;
+use App\Http\Controllers\NoticeController;
+use App\Http\Controllers\UserController;
+
+use App\Models\User;
+
+Route::apiResource('comments', CommentController::class);
+Route::apiResource('companies', CompanyController::class);
+Route::apiResource('company-deregister-requests', CompanyDeregisterRequestController::class);
+Route::apiResource('company-register-requests', CompanyRegisterRequestController::class);
+Route::apiResource('company-reviews', CompanyReviewController::class);
+Route::apiResource('notices', NoticeController::class);
+Route::apiResource('users', UserController::class);
+
+Route::post('/login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Credenciales inválidas'], 401);
+    }
+
+    $token = $user->createToken('api-token')->plainTextToken;
+    return response()->json(['token' => $token]);
+});
+
+Route::post('/logout', [Auth::logout()]);
+
+Route::middleware('auth:sanctum')->post('/tutors/import', [TutorsImportController::class, 'import']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
