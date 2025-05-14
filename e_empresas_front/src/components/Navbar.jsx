c0f3c3d5c26b3012ae3e03508ac2e3fa394e5563
alorenzot <alejandrolorenzotoledo@gmail.com>
2025-05-14 22:32:40 +0200

    Implementado logout correctamente

--- e_empresas_front/src/components/Navbar.jsx
+++ e_empresas_front/src/components/Navbar.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.scss";
 import LogoutButton from "./Logout";
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    setIsLoggedIn(!!token);
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="navbar__logo">AppEmpreses</div>
       <div className="navbar__links">
         <Link to="/" className="navbar__link">Inicio</Link>
-        <Link to="/import-tutors" className="navbar__link">Importar Tutores</Link>
-        <Link to="/user-menu" className="navbar__link">Usuarios</Link>
-        <LogoutButton/>
+
+        {!isLoggedIn &&(
+          <>
+            <Link to="/login" className="navbar__link">Iniciar Sesión</Link>
+          </>
+        )
+        }
+
+        {isLoggedIn && (
+          <>
+            <Link to="/import-tutors" className="navbar__link">Importar Tutores</Link>
+            <Link to="/user-menu" className="navbar__link">Usuarios</Link>
+            <LogoutButton />
+          </>
+        )}
       </div>
     </nav>
   );
