067e3dd00da776a2aa93ac605e00858a3c68ed21
alorenzot <alejandrolorenzotoledo@gmail.com>
2025-05-15 11:43:41 +0200

    Actualización de la implementacion de la ruta protegida

--- e_empresas_front/src/App.js
+++ e_empresas_front/src/App.js
@@ -1,57 +1,71 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import UserPage from './pages/UserPage';
-import ProtectedRoute from './components/ProtectedRoute';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import ImportTutors from './pages/ImportTutors';
-import UserForm from './components/UserForm';
-import ViewUser from './components/ViewUser';
-import EditUser from './components/EditUser';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import UserPage from "./pages/UserPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import ImportTutors from "./pages/ImportTutors";
+import UserForm from "./components/UserForm";
+import ViewUser from "./components/ViewUser";
+import EditUser from "./components/EditUser";
+import CompanyManager from "./pages/CompanyManager";
 
 function App() {
   return (
     <Router>
-    <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
 
         <Route
           path="/user-menu"
           element={
-            <ProtectedRoute requiredRole="admin" element={<UserPage />} />
+            <ProtectedRoute requiredRoles={["admin"]} element={<UserPage />} />
           }
         />
 
         <Route
           path="/users/:id"
           element={
-            <ProtectedRoute requiredRole="admin" element={<ViewUser />} />
+            <ProtectedRoute requiredRoles={["admin"]} element={<ViewUser />} />
           }
         />
 
         <Route
           path="/users/:id/edit"
           element={
-            <ProtectedRoute requiredRole="admin" element={<EditUser />} />
+            <ProtectedRoute requiredRoles={["admin"]} element={<EditUser />} />
           }
         />
 
         <Route
           path="/add-user"
           element={
-            <ProtectedRoute requiredRole="admin" element={<UserForm />} />
+            <ProtectedRoute requiredRoles={["admin"]} element={<UserForm />} />
           }
         />
 
         <Route
           path="/import-tutors"
           element={
-            <ProtectedRoute requiredRole="admin" element={<ImportTutors />} />
+            <ProtectedRoute
+              requiredRoles={["admin"]}
+              element={<ImportTutors />}
+            />
           }
         />
-        
+
+        <Route
+          path="/company-menu"
+          element={
+            <ProtectedRoute
+              requiredRoles={["admin"]}
+              element={<CompanyManager />}
+            />
+          }
+        />
+
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
