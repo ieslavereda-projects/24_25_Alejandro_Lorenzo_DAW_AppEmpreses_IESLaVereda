efca5aff8112c92365ffa6d18ac2fedb1ec10ca9
alorenzot <alejandrolorenzotoledo@gmail.com>
2025-06-09 22:34:48 +0200

    Aumentado el tamaño de fuente

--- e_empresas_front/src/components/StarRating.jsx
+++ e_empresas_front/src/components/StarRating.jsx
@@ -2,7 +2,7 @@ const StarRating = ({ value }) => {
     const stars = Array.from({ length: 5 }, (_, i) => (
       <span key={i}>{i < value ? '★' : '☆'}</span>
     ));
-    return <span className="stars fs-1">{stars}</span>;
+    return <span className="stars fs-3">{stars}</span>;
   };
   
   export default StarRating;
